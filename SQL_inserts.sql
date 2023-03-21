a97b6a52283f9885f704a6c01242f5973cceaf84
Baste Olafsen <basteho@stud.ntnu.no>
2023-03-21 13:18:43 +0100

    Oppdatering

--- SQL_inserts.sql
+++ SQL_inserts.sql
@@ -26,8 +26,9 @@ INSERT INTO "main"."Delstrekning"
 ("sportype", "lengde", "stasjon1", "stasjon2")
 VALUES ("enkelt", 170, "Mo i Rana", "Fauske");
 
-INSERT INTO "main"."Delstrekning" ("delstrekningID", "sportype", "lengde", "stasjon1", "stasjon2") VALUES ('enkelt', '60', 'Fauske', 'Bodø');
-
+INSERT INTO "main"."Delstrekning"
+("sportype", "lengde", "stasjon1", "stasjon2")
+VALUES ("enkelt", 60, "Fauske", "Bodø");
 
 INSERT INTO "main"."HarDelstrekning"
 ("strekningID", "delstrekningID")
@@ -279,3 +280,219 @@ VALUES (3, 2, 1231, 0914);
 INSERT INTO "main"."KjørerStrekning"
 ("ruteID", "delstrekningID", "tidStasjon1", "tidStasjon2")
 VALUES (3, 3, 0914, 0811);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 12);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 12);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 12);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 12);
+
+INSERT INTO "main"."TogruteForekomst"
+("ruteID", "dato")
+VALUES (1, 1680472800);
+
+INSERT INTO "main"."TogruteForekomst"
+("ruteID", "dato")
+VALUES (1, 1680559200);
+
+INSERT INTO "main"."TogruteForekomst"
+("ruteID", "dato")
+VALUES (2, 1680472800);
+
+INSERT INTO "main"."TogruteForekomst"
+("ruteID", "dato")
+VALUES (2, 1680559200);
+
+INSERT INTO "main"."TogruteForekomst"
+("ruteID", "dato")
+VALUES (3, 1680472800);
+
+INSERT INTO "main"."TogruteForekomst"
+("ruteID", "dato")
+VALUES (3, 1680559200);
