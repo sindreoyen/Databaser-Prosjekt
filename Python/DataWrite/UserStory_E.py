4b572fb903000a9e2403a0fc23f19e31c73f4610
Eivind Sætre <eivsaetr@stud.ntnu.no>
2023-03-21 14:56:51 +0100

    Update UserStory_E.py

--- Python/DataWrite/UserStory_E.py
+++ Python/DataWrite/UserStory_E.py
@@ -5,19 +5,16 @@ connection = sqlite3.connect("tog_v2.db")
 cursor = connection.cursor()
 ############################################
 
-#navn = input("Skriv inn navnet ditt: ")
-#epost = input("Skriv inn din epost: ")
-#mobilnr = input("Skriv inn ditt mobilnummer: ")
+navn = input("Skriv inn navnet ditt: ")
+epost = input("Skriv inn din epost: ")
+mobilnr = input("Skriv inn ditt mobilnummer: ")
 
-##try:
-#cursor.execute("""INSERT INTO Kunde
-    #VALUES(NULL, ?,?,?)""",
-#(navn, epost, mobilnr))
-##except Exception:
-    ##print("Something went wrong")
-cursor.execute("SELECT * FROM Kunde")
-kunder = cursor.fetchall()
-print(kunder)
+try:
+    cursor.execute("""INSERT INTO Kunde
+    VALUES(NULL, ?,?,?)""",
+    (navn, epost, mobilnr))
+except Exception:
+    print("Something went wrong")
 
 ### Adding changes ###
 connection.commit()
