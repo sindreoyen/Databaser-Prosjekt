55e79ead0f3ef2aa074ff49ae3a232047ea89c87
Eivind Sætre <eivsaetr@stud.ntnu.no>
2023-03-21 12:18:23 +0100

    Update sete-sql.sql

---
+++ sete-sql.sql
@@ -0,0 +1,191 @@
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (1, 12);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (2, 12);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (3, 12);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 1);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 2);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 3);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 4);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 5);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 6);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 7);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 8);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 9);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 10);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 11);
+
+INSERT INTO "main"."Sete"
+("vognID", "seteNR")
+VALUES (4, 12);
