77060fd3542f1e6e3c21db21c8c8a0a79627789c
Baste Olafsen <basteho@stud.ntnu.no>
2023-03-21 14:44:32 +0100

    Yup

---
+++ Python/DataWrite/userStory_d.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+### Connecting to database #################
+connection = sqlite3.connect("tog_v2.db")
+cursor = connection.cursor()
+############################################
+
+stationList = []
+for row in cursor.execute("""SELECT Delstrekning.stasjon1, Delstrekning.stasjon2
+FROM Delstrekning
+NATURAL JOIN HarDelstrekning
+NATURAL JOIN Banestrekning
+WHERE Banestrekning.navn = 'Nordlandsbanen'"""):
+    print(cursor.fetchall)
+    stationList.append(cursor.fetchone)
+    print(stationList)
+
+
+
+
+
+### Adding changes ###
+connection.commit()
+connection.close()
+######################
