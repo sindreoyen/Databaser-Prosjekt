f1726e7204aab9e2fbc799290967de771e5fc112
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-03-22 21:07:06 +0100

    Almost finished seat tickets

--- Python/DB2tasks/UserStory_G.py
+++ Python/DB2tasks/UserStory_G.py
@@ -30,7 +30,7 @@ if choice.lower() == "s":
     ids = Utilities.getTogruteWithDir(connection=connection, dir=res[2])
     G_methods.seats.orderSeat(connection=connection, ids=ids,
                               startStation=res[0], endStation=res[1],
-                              withMainDir=res[2])
+                              withMainDir=res[2], customerID=user[0])
 elif choice.lower() == "k":
     G_methods.coupes.orderCoupe(connection=connection)
     
