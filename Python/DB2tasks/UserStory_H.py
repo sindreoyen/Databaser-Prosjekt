2ca8a019ae920131712d5f249bb6c4e4170bdf9f
Eivind Sætre <eivsaetr@stud.ntnu.no>
2023-03-22 15:52:58 +0100

    H

--- Python/DB2tasks/UserStory_H.py
+++ Python/DB2tasks/UserStory_H.py
@@ -1,1 +1,24 @@
-print("Ikke laget ferdig")
+import sqlite3
+
+### Connecting to database #################
+connection = sqlite3.connect("tog_v3.db")
+cursor = connection.cursor()
+############################################
+
+epost = input("Skriv inn din epost som du er oppgitt med hos oss: ")
+
+cursor.execute("SELECT epost FROM Kunde")
+eposter_temp = cursor.fetchall()
+eposter = []
+
+for row in eposter_temp:
+    eposter.append(row[0])
+if epost not in eposter:
+    raise Exception("Epost finnes ikke i kunderegisteret")
+
+1679475912
+
+### Adding changes ###
+connection.commit()
+connection.close()
+######################
