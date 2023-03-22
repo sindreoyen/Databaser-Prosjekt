27c95ab4d24c8841db57840857c04c03ffc81c28
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-03-22 14:13:19 +0100

    Change main.py + begin userstory G

--- Python/DB2tasks/UserStory_G.py
+++ Python/DB2tasks/UserStory_G.py
@@ -1,2 +1,21 @@
-## Check if email is registred 
+import LocalData
+import Validators
+
 print("Ikke laget ferdig")
+
+### Connecting to database #################
+connection = LocalData.getDBConnection()
+cursor = connection.cursor()
+############################################
+
+user: tuple = None
+while user == None:
+    email = input("Hei! Vennligst skriv eposten til brukeren du er registrert med: ")
+    user = Validators.fetchUser(email=email, connection=connection)
+
+
+
+
+### Cose the connection ####################
+connection.close()
+############################################
