8d60efed41595a0701ac35703412bc0d3cd2895d
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-03-23 12:40:46 +0100

    fix: c

--- Python/DB2tasks/UserStory_C.py
+++ Python/DB2tasks/UserStory_C.py
@@ -27,10 +27,11 @@ weekdayDict: dict = {
     "søndag": "søn"
 }
 weekday: str = ""
+weekday_long: str = ""
 while True:
     try: 
-        weekday = input("Hvilken ukedag ønsker du å sjekke togreiser for?: ")
-        weekday = weekdayDict[weekday.lower()]
+        weekday_long = input("Hvilken ukedag ønsker du å sjekke togreiser for?: ")
+        weekday = weekdayDict[weekday_long.lower()]
         break
     except: 
         print("Feil format!")
@@ -38,17 +39,18 @@ while True:
 
 
 try:
-    for row in cursor.execute("""SELECT DISTINCT ruteID
+    for row in cursor.execute("""SELECT DISTINCT Togrute.ruteID, Tf.navn
                       FROM Togrute 
                       NATURAL JOIN (SELECT KjørerStrekning.ruteID, KjørerStrekning.delstrekningID, Delstrekning.stasjon1, Delstrekning.stasjon2
                                    FROM KjørerStrekning
                                    NATURAL JOIN Delstrekning
                                    WHERE Delstrekning.stasjon1 = ? OR Delstrekning.stasjon2 = ?)
                       NATURAL JOIN GårPåUkedag
+                      LEFT JOIN TogruteForekomst Tf ON Togrute.ruteID=Tf.ruteID
                       WHERE GårPåUkedag.navn = ?""",
                    (station, station, weekday)):
-        print(row)
-        print("##### TODO: Fiks bedre output her")
+        try: print("Togrute " + str(row[0]) + ",", str(row[1]) + ", går innom " + station, "på", str() + "er.")
+        except: print("Noe gikk galt i visning av togrute.")
     
 except Exception:
     print("Ugyldig input")
