028ab01ee8bfdd0ea80434218e269386444892d0
Eivind Sætre <eivsaetr@stud.ntnu.no>
2023-03-23 08:31:03 +0100

    Some shit

--- Python/DB2tasks/UserStory_H.py
+++ Python/DB2tasks/UserStory_H.py
@@ -1,25 +1,45 @@
+from lib2to3.pgen2.pgen import DFAState
+from multiprocessing import connection
 import sqlite3
 import LocalData
 
 
 ### Connecting to database #################
-connection = LocalData.getDBConnection()
+##connection = LocalData.getDBConnection()
+connection = sqlite3.connect("b&e.db")
 cursor = connection.cursor()
 ############################################
 
-epost = input("Skriv inn din epost som du er oppgitt med hos oss: ")
 
-cursor.execute("SELECT epost FROM Kunde")
-eposter_temp = cursor.fetchall()
-eposter = []
+##Validate if epost exist in db
+##epost = input("Skriv inn din epost som du er oppgitt med hos oss: ")
 
-for row in eposter_temp:
-    eposter.append(row[0])
-if epost not in eposter:
-    raise Exception("Epost finnes ikke i kunderegisteret")
+# cursor.execute("SELECT epost FROM Kunde")
+# eposter_temp = cursor.fetchall()
+# eposter = []
 
-1679475912
+# for row in eposter_temp:
+#     eposter.append(row[0])
+# if epost not in eposter:
+#     raise Exception("Epost finnes ikke i kunderegisteret")
 
+##make station list
+
+
+## Find departure time and arrival 
+
+cursor.execute("""SELECT MAX(MAX(KjørerStrekning.tidStasjon1), MAX(KjørerStrekning.tidStasjon2)), MIN(MIN(KjørerStrekning.tidStasjon1), MIN(KjørerStrekning.tidStasjon2))
+FROM Kundeordre
+NATURAL Join Kunde
+NATURAL JOIN Togrute
+NATURAL JOIN Setebillett
+NATURAL JOIN SetebillettIOrdre
+NATURAL JOIN Delstrekning
+NATURAL JOIN KjørerStrekning """)
+times = cursor.fetchall()
+departureTime = times[0][1]
+arrivalTime = times [0][0]
+print(departureTime, arrivalTime)
 ### Adding changes ###
 connection.commit()
 connection.close()
