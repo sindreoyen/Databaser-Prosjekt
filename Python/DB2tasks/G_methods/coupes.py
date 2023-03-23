ca80e683ab20aaf7de299bec58b0970932d5a962
Sindre Øyen <31347883+sindreoyen@users.noreply.github.com>
2023-03-23 10:43:14 +0100

    Update coupes.py

--- Python/DB2tasks/G_methods/coupes.py
+++ Python/DB2tasks/G_methods/coupes.py
@@ -2,3 +2,12 @@ import sqlite3
 
 def orderCoupe(connection: sqlite3.Connection):
     print("Coupe")
+    ### TODO: La bruker velge stasjoner
+    ### TODO: Finn ruter med sovekupé
+    ### TODO: La bruker velge mellom disse
+    ### TODO: La bruker velge kupe
+    ### TODO: La bruker velge seng / senger
+    
+    ### TODO: Opprett kundeordre
+    ### TODO: Lag billett til hver seng
+    ### TODO: Knytt billetter og ordre sammen med relasjoner
